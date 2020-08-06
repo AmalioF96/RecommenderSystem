024d1cba7915eb51db05dc5f7d3daaa5ec11240f
AmalioF96 <amaliocabeza.16@gmail.com>
2020-08-06 10:58:46 +0200

    Reestructura codigo sacar distancia euclidea

--- preprocesado/ClientePorSemana.py
+++ preprocesado/ClientePorSemana.py
@@ -8,13 +8,21 @@ import numpy as np
 from modelo.persistencia.Connection import Connection
 
 import datetime
-from mock.mock import inplace
+
+
 # Constantes
 URL = 'bolt://localhost:11005'
 USER = 'neo4j'
 PASSWORD = '123'
 myConect = Connection(URL, USER, PASSWORD)
 
+def getComprasArticulosPorSemana(minDay,maxDay):
+    '''
+    El menor dia es 1 y el mayor 31
+    '''
+    query="match (c:cliente)-[r:COMPRA]-(f:factura)-[r2:CONTIENE]-(a:articulo) WHERE toInteger(substring(r.fecha,0,2))>={min} AND toInteger(substring(r.fecha,0,2))<={max} return a.id_interno as articulo, count(a) as compras order by articulo".format(min=minDay,max=maxDay)
+    data = pd.DataFrame(myConect.consultar(query), columns=['articulo','compras'])
+    return data
 
 def getClientesPorSemanaConFecha(fecha="01/07/2018"):
     
@@ -43,14 +51,14 @@ def getArticulosPorSemana(minDay,maxDay):
     '''
     El menor dia es 1 y el mayor 31
     '''
-    query="match (c:cliente)-[r:COMPRA]-(f:factura)-[r2:CONTIENE]-(a:articulo) WHERE c.id<>'1715' AND toInteger(substring(r.fecha,0,2))>={min} AND toInteger(substring(r.fecha,0,2))<={max} return DISTINCT(a.descripcion) as articulo".format(min=minDay,max=maxDay)
+    query="match (c:cliente)-[r:COMPRA]-(f:factura)-[r2:CONTIENE]-(a:articulo) WHERE c.id<>'1715' AND toInteger(substring(r.fecha,0,2))>={min} AND toInteger(substring(r.fecha,0,2))<={max} return DISTINCT(a.id_interno) as articulo".format(min=minDay,max=maxDay)
     data = pd.DataFrame(myConect.consultar(query), columns=['articulo'])
     return data
 
 def getComprasPorCliente(minDay,maxDay):
     '''
     '''
-    query="match (c:cliente)-[r:COMPRA]-(f:factura)-[r2:CONTIENE]-(a:articulo) WHERE c.id<>'1715' AND toInteger(substring(r.fecha,0,2))>={min} AND toInteger(substring(r.fecha,0,2))<={max} return c.id as cliente, a.descripcion as articulo, count(a) as compras".format(min=minDay,max=maxDay)
+    query="match (c:cliente)-[r:COMPRA]-(f:factura)-[r2:CONTIENE]-(a:articulo) WHERE c.id<>'1715' AND toInteger(substring(r.fecha,0,2))>={min} AND toInteger(substring(r.fecha,0,2))<={max} return c.id as cliente, a.id_interno as articulo, count(a) as compras".format(min=minDay,max=maxDay)
     data = pd.DataFrame(myConect.consultar(query), columns=['cliente','articulo','compras'])
     return data
 
@@ -60,7 +68,6 @@ dayMax=7
 #1# - Recogemos datos  
 clientesPrimeraSemana=getClientesPorSemana(dayMin, dayMax)
 articulosPrimeraSemana=getArticulosPorSemana(dayMin,dayMax)
-
 #2# - Pasamos los datos a lista
 clientes = sum(clientesPrimeraSemana.values.tolist(), [])
 articulos = sum(articulosPrimeraSemana.values.tolist(), [])
@@ -70,11 +77,31 @@ dfObj = pd.DataFrame(columns=articulos, index=clientes) #Usamos el numero de cli
 
 #4# - Obtener el contenido de dfObj
 clienteXarticulo=getComprasPorCliente(dayMin,dayMax)
+comprasPrimeraSemana=getComprasArticulosPorSemana(dayMin, dayMax)
+print("\n### comprasPrimeraSemana ###\n",comprasPrimeraSemana,"###\n")
+print("\n### clienteXarticulo ###\n",clienteXarticulo,"###\n")
+
+
+
 
 #5# - Rellenamos la matriz dfObj     
 for indexI,i in enumerate(clienteXarticulo['cliente']):
-    dfObj.loc[clienteXarticulo.loc[indexI,'cliente']][clienteXarticulo.loc[indexI,'articulo']] =  clienteXarticulo.loc[indexI,'compras']
+    
+    
+    cliente=clienteXarticulo.loc[indexI,'cliente']
+    articulo=clienteXarticulo.loc[indexI,'articulo']
+    comprasCliente=clienteXarticulo.loc[indexI,'compras']
+    #
+    aux=comprasPrimeraSemana.loc[comprasPrimeraSemana['articulo'] == articulo, ['compras']]
+    comprasTotales=aux.iloc[0].loc["compras"]
+    #
+    x=comprasCliente/comprasTotales
+    
+    dfObj.loc[cliente][articulo] = x
 
+    if indexI%1000==0:
+        print("Iteracion: ",indexI,"\tCliente: ",cliente,'\tArticulo: ',articulo,'\tcomprasCliente: ',comprasCliente, "\tComprasTotales: ",comprasTotales, "\t X: ",x)
+    
 
 print(dfObj)
 
--- recomendaciones/CMF.py
+++ recomendaciones/CMF.py
@@ -46,6 +46,13 @@ def getClientesMarca():
     data = pd.DataFrame(myConect.consultar(query), columns=['cliente', 'marca', 'similitud'])
     return data
 
+def getClientesMarcaFamilia():
+    query="MATCH (c1:cliente)-[ra1:COMPRA]->(f1:factura)-[ra2:CONTIENE]->(a:articulo) where c1.id<>'1715' return c1.id as cliente,a.marca_id as marca, a.familia_id as familia, count(a.marca_id) as similitud order by cliente desc"
+    
+    print(query)
+    data = pd.DataFrame(myConect.consultar(query), columns=['cliente', 'marca','familia', 'similitud'])
+    return data
+
 def getMarcasFamilia():
     query="MATCH (a:articulo) return a.marca_id as marca, a.familia_id as familia, count(a.familia_id) as fitness order by fitness desc, marca,familia"
     
@@ -57,35 +64,29 @@ def getMarcasFamilia():
 numClientes = sum(getClientes().values.tolist(), [])
 numFamilias = sum(getFamilias().values.tolist(), [])
 numMarcas = sum(getMarcas().values.tolist(), [])
-#Producto cartesiano de todas las marcas y todas las familias
-marcasXFamilia=[(m+f) for m in numMarcas for f in numFamilias]
 #2# - Formamos Matriz")
 #Usamos el numero de clientes(filas) y el numero de familias(columnas) para formar la matriz
 
-dfObj = pd.DataFrame(columns=marcasXFamilia, index=numClientes) 
-
+dfObj=pd.MultiIndex.from_product([numClientes,numMarcas,numFamilias])
 #3# - Recogemos getClienteFamilia     
-cliXfam = getClientesFamilia()
+#cliXfam = getClientesFamilia()
+cliXmarXfam=getClientesMarcaFamilia()
 #4# - Recogemos getClienteMarca    
-cliXmar = getClientesMarca()
+#cliXmar = getClientesMarca()
 #5# - Recogemos getMarcaFamilia     
 marXfam = getMarcasFamilia()
-print(cliXfam)
+print(cliXmarXfam[:4])
 
 #6# - Recorremos matriz dfObj
-for indexI,i in enumerate(cliXfam['cliente']):
-    nombreMF=cliXmar.loc[indexI,'marca']+cliXfam.loc[indexI,'familia']
-    
-    cm=cliXmar.loc[indexI,'similitud']
-    cf=cliXfam.loc[indexI,'similitud']
-    
-    mf=marXfam.loc[cliXmar.loc[indexI,'marca'], cliXfam.loc[indexI,'familia'], 'similitud']
-    
-    dfObj.loc[cliXfam.loc[indexI,'cliente']][mf] = (cm+cf)%mf
+for indexI,i in enumerate(cliXmarXfam['cliente']):
+    #nombreMF=cliXmar.loc[indexI,'marca']+cliXfam.loc[indexI,'familia']
+
+            mf=marXfam.loc[cliXmarXfam.loc[indexI,'marca']][cliXmarXfam.loc[indexI,'familia']]
+            dfObj.loc[cliXmarXfam.loc[indexI,'cliente']][cliXmarXfam.loc[indexI,'marca']][cliXmarXfam.loc[indexI,'familia']] = (cliXmarXfam.loc[indexI,'similitud'])%mf
 
 
 print(dfObj)
-#CM[C][M] + CF [C][F] % MF[M][F]
+#CM + CF  % MF
 
 
 
