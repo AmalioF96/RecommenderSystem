6d8f024449413f6345339091c329973dea48a905
AmalioF96 <amaliocabeza.16@gmail.com>
2020-11-09 18:49:10 +0100

    Controlador para la api del sistema recomendador

---
+++ api/appflask/ApiController.py
@@ -0,0 +1,18 @@
+'''
+Created on 9 nov. 2020
+
+@author: Amalio
+'''
+
+class ApiController(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+    def recommend(self, id):
+        pass
