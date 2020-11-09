996ad71ae1bf4454a57ebc88b4c282e641062e41
AmalioF96 <amaliocabeza.16@gmail.com>
2020-11-09 18:49:57 +0100

    Creación del modelo que almacena las consulta del sistema recomendador

---
+++ modelo/persistencia/recomender/recommendQuerys.py
@@ -0,0 +1,18 @@
+'''
+Created on 9 nov. 2020
+
+@author: Amalio
+'''
+from modelo.persistencia.bbdd.Connection import Connection as myConect
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
